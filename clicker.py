039c744390a77d3c4b91afe4ab81c984a78a2833
NSP909 <nspriyadarshan@gmail.com>
2024-03-06 00:55:16 -0500

    lol

--- clicker.py
+++ clicker.py
@@ -5,8 +5,9 @@ import pyautogui
 import time
 
 def openWeb(url):
+    webbrowser.open("https://www.tutorialspoint.com/whiteboard.htm", new=2)
     webbrowser.open(url, new=2)
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.click(1060,880)
     pyautogui.typewrite("Lyle241")
     pyautogui.click(1060,980)
