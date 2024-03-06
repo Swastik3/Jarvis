54ba5cf69b31b1724af19000b5180569bb05b36a
Dahong Luo <dappon.04@gmail.com>
2024-03-06 01:06:36 -0500

    fixed lagging issue

--- clicker.py
+++ clicker.py
@@ -3,11 +3,16 @@ from pynput.keyboard import Key, Controller
 import mouse
 import pyautogui
 import time
+import subprocess
 
 def openWeb(url):
-    webbrowser.open("https://www.tutorialspoint.com/whiteboard.htm", new=2)
-    webbrowser.open(url, new=2)
-    time.sleep(1)
+    #webbrowser.open("https://www.tutorialspoint.com/whiteboard.htm", new=2)
+    #webbrowser.open(url, new=2)
+    subprocess.Popen(f"start chrome /new-tab {url}", shell = True)
+    """time.sleep(1)
     pyautogui.click(1060,880)
     pyautogui.typewrite("Lyle241")
-    pyautogui.click(1060,980)
+    pyautogui.click(1060,980)"""
+    
+if __name__ == "__main__":
+    openWeb("https://www.tutorialspoint.com/whiteboard.htm")
